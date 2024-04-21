b3a4b712bdc163e7258a8dc2e4a6c27898babfc7
Cihat SALİK <57585087+cihat@users.noreply.github.com>
2024-04-22 00:28:45 +0300

    Change framwork form Vue.js to Svelte

---
+++ src/App.svelte
@@ -0,0 +1,27 @@
+<script>
+  console.log('Hello, Svelte!')
+</script>
+
+<main class="container">
+  <canvas class="space" width="600" height="900" />
+</main>
+
+<style>
+  .container {
+    margin: auto;
+  }
+  .space {
+    position: relative;
+    display: grid;
+    justify-items: center;
+    align-items: center;
+    margin: auto;
+    border: 1px solid black;
+    grid-template-columns: repeat(3, 1fr);
+    grid-template-rows: repeat(3, 1fr);
+    gap: 10px;
+    padding: 10px;
+    border-radius: 10px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  }
+</style>
