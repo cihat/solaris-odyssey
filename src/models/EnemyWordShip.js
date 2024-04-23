c6d4cb883bd4f4c75bd1525c92a1ce9ec996bb4d
Cihat SALİK <57585087+cihat@users.noreply.github.com>
2024-04-23 22:41:26 +0300

    Updating the EnemyWordShip's views is like this for now

--- src/models/EnemyWordShip.js
+++ src/models/EnemyWordShip.js
@@ -1,12 +1,14 @@
 import { getWord } from "../data/words"
+import { generateRandomCoordinates } from "../lib/random"
 
 export default class WordShip {
   styles = {
-    color: 'white',
+    color: 'black',
     fillStyle: 'white',
     font: '20px Arial',
     textAlign: 'center',
     textBaseline: 'middle',
+    radius: 50,
   }
   coords = {
     x: 0,
@@ -15,8 +17,9 @@ export default class WordShip {
 
   constructor() {
     this.word = getWord({ level: 2 })
-    this.coords.x = (Math.random() * 500) + 25;
-    this.coords.y = (Math.random() * -50) - (Math.random() * 100); // Start from above the canvas
+    const { x, y } = generateRandomCoordinates()
+    this.coords.x = x
+    this.coords.y = y
   }
 
   generate(count) {
@@ -24,19 +27,41 @@ export default class WordShip {
     return ships;
   }
 
-  draw(ctx) {
-    ctx.fillStyle = this.styles.color;
-    ctx.font = '20px Arial';
-    ctx.fillText(this.word, this.coords.x, this.coords.y);
-    ctx.strokeStyle = this.styles.fillStyle;
+  draw(ctx, { speed }) {
+    const itemWidth = this.word.length * 10;
+    const itemHeight = this.word.length * 10;
 
-    this.coords.y += 0.5;
+    // Draw the black background for the item
+    ctx.fillRect(this.coords.x, this.coords.y, itemWidth, itemHeight);
+
+    // Draw the circle
+    ctx.beginPath();
+    ctx.arc(this.coords.x + itemWidth / 2, this.coords.y + itemHeight / 2, this.styles.radius, 0, 2 * Math.PI, false);
+    ctx.fillStyle = 'green';
+    ctx.fill();
+    ctx.lineWidth = 5;
+    ctx.strokeStyle = '#003300';
+    ctx.stroke();
+
+    // Set the text styles
+    ctx.fillStyle = this.styles.color;
+    ctx.font = this.styles.font;
+    ctx.textAlign = 'center'; // Center the text horizontally
+    ctx.textBaseline = 'middle'; // Center the text vertically
+
+    // Calculate the center position for the text
+    const centerX = this.coords.x + itemWidth / 2;
+    const centerY = this.coords.y + itemHeight / 2;
+
+    // Draw the text centered within the item
+    ctx.fillText(this.word, centerX, centerY);
+    this.coords.y += speed;
 
     // Reset position when it reaches the bottom of the canvas
     if (this.coords.y > ctx.canvas.height) {
-      this.coords.y = Math.random() * -100 - 40; // Start from above the canvas again
-      this.coords.x = (Math.random() * 600) + 50; // Randomize x position
-      // this.speed = Math.random() * 2 + 1; // Randomize speed
+      const { x, y } = generateRandomCoordinates()
+      this.coords.x = x
+      this.coords.y = y
     }
   }
 }
