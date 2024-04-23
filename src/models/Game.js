d11b4565a3ba1b37417ecf8a9d2d5e9456c9a43d
Cihat SALİK <57585087+cihat@users.noreply.github.com>
2024-04-23 22:40:23 +0300

    Add speed and enemies count dynamicly

--- src/models/Game.js
+++ src/models/Game.js
@@ -3,10 +3,25 @@ import EnemyWordShip from "./EnemyWordShip";
 import { soundManager } from "../lib/sound";
 import solarisSoundTrack from '../assets/sound/Solaris-Soundtrack.ogg';
 
+const levels = [
+  { speed: 1, numberOfEnemies: 10 },
+  { speed: 2, numberOfEnemies: 20 },
+  { speed: 3, numberOfEnemies: 30 },
+  { speed: 4, numberOfEnemies: 40 },
+  { speed: 5, numberOfEnemies: 50 },
+  { speed: 6, numberOfEnemies: 60 },
+  { speed: 7, numberOfEnemies: 70 },
+  { speed: 8, numberOfEnemies: 80 },
+  { speed: 9, numberOfEnemies: 90 },
+  { speed: 10, numberOfEnemies: 100 },
+  { speed: 11, numberOfEnemies: 110 },
+]
+
 export default class Game extends EventTarget {
   enemyWordShips = []
   spaceShip = null
-  speed = 0.3
+  speed = 0.5
+  numberOfEnemies = 10
   context
   input = ""
   onStateChange = new Event('stateChange')
@@ -14,7 +29,7 @@ export default class Game extends EventTarget {
 
   constructor(context) {
     super()
-    this.enemyWordShips = new EnemyWordShip().generate(10)
+    this.enemyWordShips = new EnemyWordShip().generate(this.numberOfEnemies)
     this.spaceShip = new SpaceShip(context)
     this.context = context
 
@@ -23,7 +38,7 @@ export default class Game extends EventTarget {
 
   draw() {
     this.spaceShip.draw(this.context)
-    this.enemyWordShips.forEach(ship => ship.draw(this.context))
+    this.enemyWordShips.forEach(ship => ship.draw(this.context, { speed: this.speed }))
   }
 
   animate() {
