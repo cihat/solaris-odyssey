64c544c88a34ba3218f0830382255cc9a093e19d
Cihat SALİK <57585087+cihat@users.noreply.github.com>
2024-04-27 22:26:58 +0300

    Reduce fire sound

--- src/models/SpaceShip.js
+++ src/models/SpaceShip.js
@@ -49,7 +49,7 @@ export default class SpaceShip {
     bullet.draw(this.context);
 
     const laserSound = new Audio(laserSoundPath);
-    laserSound.volume = 0.2;
+    laserSound.volume = 0.1;
     laserSound.play();
   }
 
